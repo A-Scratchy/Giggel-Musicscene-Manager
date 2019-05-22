5b1a5dd9e025ce85c136979f1784ff5e23651efa
Adam <ascratcherd@gmail.com>
2019-05-22 21:27:30 +0100

    Unit test that checks correct template rendered for home.html

--- giggel/main/tests.py
+++ giggel/main/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
 # unit tests for main app
+
+
+class BasicUnitTestsMain(TestCase):
+
+    def test_url_retruns_correct_template_for_homepage(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'main/home.html')
