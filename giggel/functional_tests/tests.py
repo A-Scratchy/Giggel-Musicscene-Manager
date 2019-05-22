6048c85aeb27f15d9749866454c894e9faaea87b
Adam <ascratcherd@gmail.com>
2019-05-22 21:12:21 +0100

    Functional test that checks home page is reachable on a browser and contains information.

---
+++ giggel/functional_tests/tests.py
@@ -0,0 +1,19 @@
+from django.test import LiveServerTestCase
+from selenium import webdriver
+
+
+class basicFunctionalTests(LiveServerTestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+        # test home page is loaded
+    def test_site_reachable(self):
+        browser = self.browser
+        browser.get(self.live_server_url)
+        print(self.live_server_url)
+        self.assertIn('Giggel', browser.title)
+        sleep(5)
