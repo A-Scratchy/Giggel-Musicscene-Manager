35db44d3856ded0eb02ab53b76761be6bce70cb3
Adam <ascratcherd@gmail.com>
2019-05-26 17:55:18 +0100

    Created profile model

--- giggel/user/models.py
+++ giggel/user/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    confirmation_slug = models.SlugField(max_length=80, default='')
+    confrimation_status = models.BooleanField(default=False)
+    membershipLevel = models.CharField(max_length=15, default='free')
+    county = models.CharField(max_length=40, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
