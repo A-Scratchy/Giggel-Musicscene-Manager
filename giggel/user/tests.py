15deece66ca7d83ad810d4c3c7d61c57ffa0fbf0
Adam <ascratcherd@gmail.com>
2019-05-26 11:38:35 +0100

    created unit test to check correct template used for register page.

--- giggel/user/tests.py
+++ giggel/user/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
-# Create your tests here.
+# Unit tests for user app
+
+
+class BasicUnitTestsUsers(TestCase):
+
+    def test_user_register_url_retruns_correct_template(self):
+        response = self.client.get('/register/')
+        self.assertTemplateUsed(response, 'users/register.html')
