6332d676bd0b185b99ec5ca4793d9b4c975034a1
Adam <ascratcherd@gmail.com>
2019-05-26 11:04:42 +0100

    added functional test to check if user can register to site

--- giggel/functional_tests/tests.py
+++ giggel/functional_tests/tests.py
@@ -1,11 +1,20 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
+import string
+import random
 
 
 class basicFunctionalTests(LiveServerTestCase):
 
+        # helper method - random string generator
+    def generate_string(self, length):
+        randString = ''.join(random.choices(
+            string.ascii_uppercase + string.digits, k=length))
+        return randString
+
     def setUp(self):
         self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(5)
 
     def tearDown(self):
         self.browser.quit()
@@ -15,3 +24,19 @@ class basicFunctionalTests(LiveServerTestCase):
         browser = self.browser
         browser.get(self.live_server_url)
         self.assertIn('Giggel', browser.title)
+
+    def test_user_can_create_account(self):
+        username = self.generate_string(9)
+        password = self.generate_string(9)
+        browser = self.browser
+        url = self.live_server_url + '/register/'
+        # print URL to console for debug
+        print(url)
+        browser.get(url)
+        browser.find_element_by_id("id_username").send_keys(username)
+        browser.find_element_by_id("id_password1").send_keys(password)
+        browser.find_element_by_id("id_password2").send_keys(password)
+        browser.find_element_by_id("submit").click()
+        time.sleep(self.SLEEP_TIME)
+        welcome = browser.find_element_by_id("welcome").text
+        assertIn(username, welcome)
