528356825c354defe3106296981d6d1cbe6009af
Adam <ascratcherd@gmail.com>
2019-06-03 19:54:05 +0100

    Refactored basicFunctionalTests. reduced execution time from 65 to 43 secs.

--- giggel/functional_tests/test_basic.py
+++ giggel/functional_tests/test_basic.py
@@ -1,6 +1,7 @@
 from django.test import LiveServerTestCase
 from django.urls import reverse
 from django.contrib.auth.models import User
+from selenium.webdriver.firefox.options import Options
 from selenium import webdriver
 from .helper_methods import helperMethods
 
@@ -8,11 +9,31 @@ from .helper_methods import helperMethods
 class basicFunctionalTests(LiveServerTestCase):
 
     def setUp(self):
-        self.browser = webdriver.Firefox()
+        options = Options()
+        options.set_headless(headless=True)
+        self.browser = webdriver.Firefox(
+            firefox_options=options)
         self.browser.implicitly_wait(5)
+        self.new_username = helperMethods.generate_string(9)
+        self.new_password = helperMethods.generate_string(9)
 
     def tearDown(self):
         self.browser.quit()
+
+    def fillInRegForm(self, username, password1, password2):
+        url = self.live_server_url + reverse('register')
+        self.browser.get(url)
+        self.browser.find_element_by_id(
+            "id_username").send_keys(username)
+        self.browser.find_element_by_id(
+            "id_password1").send_keys(password1)
+        self.browser.find_element_by_id(
+            "id_password2").send_keys(password2)
+        self.browser.find_element_by_id(
+            "id_county").send_keys('Yorkshire')
+        self.browser.find_element_by_id(
+            "id_birth_date").send_keys('1991-06-02')
+        self.browser.find_element_by_id("submit").click()
 
     def test_home_page_is_loaded(self):
         self.browser.get(self.live_server_url)
@@ -23,43 +44,16 @@ class basicFunctionalTests(LiveServerTestCase):
         self.assertTrue(self.browser.find_element_by_name("login"))
 
     def test_user_can_create_account(self):
-        url = self.live_server_url + reverse('register')
-        new_username = helperMethods.generate_string(9)
-        new_password = helperMethods.generate_string(9)
-        self.browser.get(url)
-        self.browser.find_element_by_id("id_username").send_keys(new_username)
-        self.browser.find_element_by_id(
-            "id_password1").send_keys(new_password)
-        self.browser.find_element_by_id(
-            "id_password2").send_keys(new_password)
-        self.browser.find_element_by_id(
-            "id_county").send_keys('Yorkshire')
-        self.browser.find_element_by_id(
-            "id_birth_date").send_keys('1991-06-02')
-        self.browser.find_element_by_id("submit").click()
+        self.fillInRegForm(self.new_username,
+                           self.new_password, self.new_password)
         welcome = self.browser.find_element_by_id("messages").text
-        self.assertIn(new_username, welcome)
-        self.assertIn(new_username, self.browser.title)
+        self.assertIn(self.new_username, welcome)
+        self.assertIn(self.new_username, self.browser.title)
         self.assertIn('Profile', self.browser.title)
 
     def test_register_form_returns_with_errors_if_invalid(self):
-        url = self.live_server_url + reverse('register')
-        new_username = helperMethods.generate_string(9)
-        self.browser.get(url)
-        self.browser.find_element_by_id("id_username").send_keys(new_username)
-        self.browser.find_element_by_id(
-            "id_password1").send_keys('test1')
-        self.browser.find_element_by_id(
-            "id_password2").send_keys('test2')
-        self.browser.find_element_by_id(
-            "id_county").send_keys('Yorkshire')
-        self.browser.find_element_by_id(
-            "id_birth_date").send_keys('1991-06-02')
-        self.browser.find_element_by_id("submit").click()
+        self.fillInRegForm(self.new_username,
+                           self.new_password, 'BadPassword')
         self.assertIn('Register', self.browser.title)
         messages = self.browser.find_element_by_id("messages").text
         self.assertIn('contains errors', messages)
-
-    def test_cannot_access_profile_when_not_logged_in(self):
-        url = self.live_server_url + reverse('profile')
-        self.assertNotIn('Profile', self.browser.title)
