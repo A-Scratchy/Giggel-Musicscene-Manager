8177de064967205799d116dfc751905d6ee1f3b3
scratchy <scratchy@rutabaga.groudon>
2019-06-24 22:43:58 +0100

    Bug in one of the tests where it doesn/'t pick up the correct user PK. need to look at fixtures

--- giggel/functional_tests/test_user.py
+++ giggel/functional_tests/test_user.py
@@ -4,6 +4,7 @@ from django.test import Client
 from django.contrib.auth.models import User
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
+import time
 
 
 class TestAnonUser(LiveServerTestCase):
@@ -54,7 +55,7 @@ class TestNewUser(LiveServerTestCase):
         self.browser = webdriver.Firefox(
             firefox_options=options)
         self.browser.implicitly_wait(1)
-        self.user = User.objects.get(pk=1)
+        self.user = User.objects.get(username='TestUser1')
 
     def test_user_sent_to_profile_after_logging_in(self):
         self.browser.get(self.live_server_url)
@@ -68,33 +69,32 @@ class TestNewUser(LiveServerTestCase):
         self.assertIn('Profile', self.browser.title)
 
 
-class TestLoggedInUSer(LiveServerTestCase):
+class TestLoggedInUser(LiveServerTestCase):
     fixtures = ['user_fixtures.json']
 
     def setUp(self):
         options = Options()
-        options.set_headless(headless=True)
+        #options.set_headless(headless=True)
         self.browser = webdriver.Firefox(
             firefox_options=options)
         self.browser.implicitly_wait(1)
         # get a user and make sure they are logged in for each test
-        self.user = User.objects.get(pk=1)
+        self.user = User.objects.get(username='TestUser1')
         client = Client()
         client.force_login(self.user)
         self.browser.get(self.live_server_url)
-        self.browser.add_cookie({'name': 'sessionid',
-            'value': client.cookies.get('sessionid').value})
-
+        self.browser.add_cookie(
+                {'name': 'sessionid',
+                 'value': client.cookies.get('sessionid').value
+                 })
 
     def tearDown(self):
         self.browser.quit()
-
 
     def test_site_indicates_user_logged_in(self):
         self.browser.get(self.live_server_url)
         self.assertIn(self.user.username, self.browser.find_element_by_name(
             "loggedInUser").text)
-
 
     def test_user_sees_correct_info_on_profile(self):
         self.browser.get(self.live_server_url + reverse('profile'))
@@ -102,7 +102,6 @@ class TestLoggedInUSer(LiveServerTestCase):
             self.user.username, self.browser.find_element_by_id("username").text)
         self.assertIn('testuser1@test.com',
                       self.browser.find_element_by_id("email").text)
-
 
     def test_user_can_update_profile(self):
         new_county = 'Lincolnshire'
@@ -115,6 +114,7 @@ class TestLoggedInUSer(LiveServerTestCase):
         county = self.browser.find_element_by_id("id_county")
         county.clear()
         county.send_keys(new_county)
+        time.sleep(20)
         self.browser.find_element_by_id("submit").click()
         # check we have gone back to the profile detail page and new email is present
         self.assertIn('User Profile', self.browser.title)
@@ -127,6 +127,7 @@ class TestLoggedInUSer(LiveServerTestCase):
         self.assertIn('testuser1@test.com',
                       self.browser.find_element_by_id("email").text)
         self.browser.find_element_by_id("updateProfile").click()
+        time.sleep(20)
         dob = self.browser.find_element_by_id("id_birth_date")
         dob.clear()
         dob.send_keys(New_DOB)
